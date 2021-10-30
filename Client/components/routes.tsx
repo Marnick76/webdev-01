d3bcb4b4190cdeee428be1cfbe8f2bcb19d4b648
Guan Hao Wu <guanhao_wu@hotmail.com>
2021-10-30 14:57:33 +0200

    Moved directories up one level

--- Client/components/routes.tsx
+++ Client/components/routes.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
-import { Layout } from './Layout';
+import { Layout } from './layout';
 import { Home } from './Home';
 
 export const routes = <Layout>
